db888e9eba4b618d75cf58edb73dcceb6761c52a
unknown <eljunz@gmail.com>
2018-08-20 07:15:32 +0800

    Updated final version

--- assets/js/main.js
+++
@@ -1,3 +0,0 @@
-snowStorm.snowColor = '#f7f7fb';   // blue-ish snow!?
-snowStorm.flakesMaxActive = 196;    // show more snow on screen at once
-snowStorm.useTwinkleEffect = true; // let the snow flicker in and out of view
